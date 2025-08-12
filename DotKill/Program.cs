abf82f6163bedac671b1b3763e6361edd9bb9875
gdesmar <75089569+gdesmar@users.noreply.github.com>
2025-08-12 15:43:25 +0000

    Wrap in giant try/catch

--- DotKill/Program.cs
+++ DotKill/Program.cs
@@ -10,28 +10,34 @@ internal class Program
 {
 	private static void Main(string[] args)
 	{
-		string loadPath = args[0];
-		ModuleDefMD module = ModuleDefMD.Load(loadPath);
-		string junkantide4dot = AntiDe4Dot.Execute(module);
-		string dedot = junkantide4dot.Split('+')[0];
-		string junk = junkantide4dot.Split('+')[1];
-		Console.WriteLine($"Anti De4Dot ........: {dedot}");
-		Console.WriteLine($"Junk ...............: {junk}");
-		Console.WriteLine($"Maths ..............: {MathProtection.Execute(module)}");
-		Console.WriteLine($"Anti Decompiler ....: {AntiDecompiler.Execute(module)}");
-		Console.WriteLine($"CFlow ..............: {CFlow.Execute(module)}");
-		string text2 = Path.GetDirectoryName(loadPath);
-		string fileName = Path.GetFileNameWithoutExtension(loadPath) + "_dotkill" + Path.GetExtension(loadPath);
-		string savePath = text2 != null ? Path.Combine(text2, fileName) : fileName;
-		module.Write(savePath, new ModuleWriterOptions(module)
+		try
 		{
-			PEHeadersOptions =
+			string loadPath = args[0];
+			ModuleDefMD module = ModuleDefMD.Load(loadPath);
+			string junkantide4dot = AntiDe4Dot.Execute(module);
+			string dedot = junkantide4dot.Split('+')[0];
+			string junk = junkantide4dot.Split('+')[1];
+			Console.WriteLine($"Anti De4Dot ........: {dedot}");
+			Console.WriteLine($"Junk ...............: {junk}");
+			Console.WriteLine($"Maths ..............: {MathProtection.Execute(module)}");
+			Console.WriteLine($"Anti Decompiler ....: {AntiDecompiler.Execute(module)}");
+			Console.WriteLine($"CFlow ..............: {CFlow.Execute(module)}");
+			string text2 = Path.GetDirectoryName(loadPath);
+			string fileName = Path.GetFileNameWithoutExtension(loadPath) + "_dotkill" + Path.GetExtension(loadPath);
+			string savePath = text2 != null ? Path.Combine(text2, fileName) : fileName;
+			module.Write(savePath, new ModuleWriterOptions(module)
 			{
-				NumberOfRvaAndSizes = 13u
-			},
-			Logger = DummyLogger.NoThrowInstance
-		});
-		Console.Write("Assembly saved .....: ");
-		Console.WriteLine(savePath);
+				PEHeadersOptions =
+				{
+					NumberOfRvaAndSizes = 13u
+				},
+				Logger = DummyLogger.NoThrowInstance
+			});
+			Console.Write("Assembly saved .....: ");
+			Console.WriteLine(savePath);
+		}
+		catch (Exception)
+		{
+		}
 	}
 }
